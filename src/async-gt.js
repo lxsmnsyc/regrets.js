ceb3fd1271576f2c98b759452479834b651605d4
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 09:58:44 +0800

    Update async-gt.js

--- src/async-gt.js
+++ src/async-gt.js
@@ -25,6 +25,7 @@
  * @author Alexis Munsayac <alexis.munsayac@gmail.com>
  * @copyright Alexis Munsayac 2019
  */
+import { all } from './utils';
 /**
  * @desc
  * Asynchronously apply an greater-than comparison.
@@ -32,5 +33,5 @@
  * @param {?Promise} b
  * @return {Promise}
  */
-const GT = (a, b) => Promise.all([a, b]).then(v => v[0] > v[1]);
+const GT = (a, b) => all([a, b]).then(v => v[0] > v[1]);
 export default GT;
