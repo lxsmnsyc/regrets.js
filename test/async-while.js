1261e4781d527821ddde2c23f7723becbb9c4b4d
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 09:47:58 +0800

    Create async-while.js

---
+++ test/async-while.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import AsyncWhile from '../src/async-while';
+
+describe('AsyncWhile', () => {
+  describe('#do', () => {
+    it('should return the same reference', () => {
+      const sample = new AsyncWhile(false);
+
+      assert(sample.do(() => {}) === sample);
+    });
+    it('should execute multiple times until the evaluator is false', (done) => {
+      let x = 0;
+
+      new AsyncWhile(() => x < 3).do(() => x++);
+
+      setTimeout(() => {
+        if (x === 3) {
+          done();
+        }
+      }, 100);
+    });
+  });
+});
