56159f99f0f2d0302ea045dbdc0dec60d2d07dcc
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 08:53:17 +0800

    Update async-ge.js

--- src/async-ge.js
+++ src/async-ge.js
@@ -32,5 +32,5 @@
  * @param {?Promise} b
  * @return {Promise}
  */
-const GreaterThanOrEqual = (a, b) => Promise.all([a, b]).then(v => v[0] >= v[1]);
-export default GreaterThanOrEqual;
+const GE = (a, b) => Promise.all([a, b]).then(v => v[0] >= v[1]);
+export default GE;
