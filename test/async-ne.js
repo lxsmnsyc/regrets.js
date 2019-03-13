d5fb2a872e4cbc749d97c897fa198e9773a4cfb4
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 09:20:22 +0800

    Update async-ne.js

--- test/async-ne.js
+++ test/async-ne.js
@@ -2,14 +2,26 @@
 import assert from 'assert';
 import NE from '../src/async-ne';
 
+/**
+ *
+ */
 describe('NE', () => {
+  /**
+   *
+   */
   it('should return a Promise', () => {
     assert(NE(true, true) instanceof Promise);
   });
+  /**
+   *
+   */
   it('should resolve to true if both values are not equal', (done) => {
-    NE(Promise.resolve(true), Promise.resolve(false)).then(x => (x ? done() : done(false)));
+    NE(true, false).then(x => (x ? done() : done(false)));
   });
+  /**
+   *
+   */
   it('should resolve to false if both values are equal', (done) => {
-    NE(Promise.resolve(true), Promise.resolve(true)).then(x => (x ? done(false) : done()));
+    NE(true, true).then(x => (x ? done(false) : done()));
   });
 });
