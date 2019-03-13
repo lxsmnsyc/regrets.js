965884e5060cbd948b6eb53851f23ed86f0d0eff
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 08:53:27 +0800

    Create async-and.js

---
+++ test/async-and.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+import assert from 'assert';
+import And from '../src/async-and';
+
+describe('And', () => {
+  it('should return a Promise', () => {
+    assert(And(true, true) instanceof Promise);
+  });
+  it('should resolve to true if both resolved values are truthy', (done) => {
+    And(Promise.resolve(true), Promise.resolve(true)).then(x => (x ? done() : done(false)));
+  });
+  it('should resolve to false if one of the values are falsey', (done) => {
+    And(Promise.resolve(true), Promise.resolve(false)).then(x => (x ? done(false) : done()));
+  });
+});
