d619bf703961c5f6145e89e77bfc5a1346e9b677
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 09:20:32 +0800

    Update async-or.js

--- test/async-or.js
+++ test/async-or.js
@@ -2,14 +2,26 @@
 import assert from 'assert';
 import Or from '../src/async-or';
 
+/**
+ *
+ */
 describe('Or', () => {
+  /**
+   *
+   */
   it('should return a Promise', () => {
     assert(Or(true, true) instanceof Promise);
   });
+  /**
+   *
+   */
   it('should resolve to true if one of the values are truthy', (done) => {
-    Or(Promise.resolve(true), Promise.resolve(true)).then(x => (x ? done() : done(false)));
+    Or(true, true).then(x => (x ? done() : done(false)));
   });
+  /**
+   *
+   */
   it('should resolve to false if both resolved values are falsey', (done) => {
-    Or(Promise.resolve(false), Promise.resolve(false)).then(x => (x ? done(false) : done()));
+    Or(false, false).then(x => (x ? done(false) : done()));
   });
 });
