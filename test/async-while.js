a0f3817b05e34899afcc2c288ef962f04131297c
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 10:20:03 +0800

    Update async-while.js

--- test/async-while.js
+++ test/async-while.js
@@ -2,24 +2,28 @@
 import assert from 'assert';
 import AsyncWhile from '../src/async-while';
 
+/**
+ *
+ */
 describe('AsyncWhile', () => {
+  /**
+   *
+   */
   describe('#do', () => {
-    it('should return the same reference', () => {
-      const sample = new AsyncWhile(false);
-
-      assert(sample.do(() => {}) === sample);
+    /**
+     *
+     */
+    it('should return a Promise', () => {
+      assert(new AsyncWhile(false).do(() => {}) instanceof Promise);
     });
+    /**
+     *
+     */
     it('should execute multiple times until the evaluator is false', (done) => {
       let x = 0;
 
       // eslint-disable-next-line no-plusplus
-      new AsyncWhile(() => x < 3).do(() => x++);
-
-      setTimeout(() => {
-        if (x === 3) {
-          done();
-        }
-      }, 100);
+      new AsyncWhile(() => x < 3).do(() => x++).then(v => (v === 3 ? done() : done(false)));
     });
   });
 });
