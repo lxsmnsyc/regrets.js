f576fe618ed551573feb606348e3fe66135f3cac
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-13 08:53:19 +0800

    Update async-gt.js

--- src/async-gt.js
+++ src/async-gt.js
@@ -32,5 +32,5 @@
  * @param {?Promise} b
  * @return {Promise}
  */
-const GreaterThan = (a, b) => Promise.all([a, b]).then(v => v[0] > v[1]);
-export default GreaterThan;
+const GT = (a, b) => Promise.all([a, b]).then(v => v[0] > v[1]);
+export default GT;
